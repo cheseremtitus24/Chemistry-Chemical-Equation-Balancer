f303be7f3ceaee76ad174f78d9e0234f8978cc5d
Cheserem Titus <50730372+cheseremtitus24@users.noreply.github.com>
2022-02-11 10:38:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import re
 from sympy import Matrix, lcm
 
+
+#TODO: use pytesseract to include image text recognition that'll help to identify and balance chemical equations
+
+
 elementList = []
 elementMatrix = []
+print("This program is derived from : https://medium.com/swlh/balancing-chemical-equations-with-python-837518c9075b")
+print('-'*22)
+print("Sample input ")
+print("Reactants: NH4NO3")
+print("Products: N2 + O2 + H2O")
+print("Balanced Equation will be = 2NH4NO3 -> 2N2 + 1O2 + 4H2O ")
+print('-'*22)
 print("please input your reactants, this is case sensitive")
 print("your input should look like: H2O+Ag3(Fe3O)4")
 reactants = input("Reactants: ")
@@ -50,25 +61,26 @@ def compoundDecipher(compound, index, side):
             multiplier = 1
         findElements(segment, index, multiplier, side)
 
+if __name__ == '__main__':
 
-for i in range(len(reactants)):
-    compoundDecipher(reactants[i], i, 1)
-for i in range(len(products)):
-    compoundDecipher(products[i], i + len(reactants), -1)
-elementMatrix = Matrix(elementMatrix)
-elementMatrix = elementMatrix.transpose()
-solution = elementMatrix.nullspace()[0]
-multiple = lcm([val.q for val in solution])
-solution = multiple * solution
-coEffi = solution.tolist()
-output = ""
-for i in range(len(reactants)):
-    output += str(coEffi[i][0]) + reactants[i]
-    if i < len(reactants) - 1:
-        output += " + "
-output += " -> "
-for i in range(len(products)):
-    output += str(coEffi[i + len(reactants)][0]) + products[i]
-    if i < len(products) - 1:
-        output += " + "
-print(output)
+    for i in range(len(reactants)):
+        compoundDecipher(reactants[i], i, 1)
+    for i in range(len(products)):
+        compoundDecipher(products[i], i + len(reactants), -1)
+    elementMatrix = Matrix(elementMatrix)
+    elementMatrix = elementMatrix.transpose()
+    solution = elementMatrix.nullspace()[0]
+    multiple = lcm([val.q for val in solution])
+    solution = multiple * solution
+    coEffi = solution.tolist()
+    output = ""
+    for i in range(len(reactants)):
+        output += str(coEffi[i][0]) + reactants[i]
+        if i < len(reactants) - 1:
+            output += " + "
+    output += " -> "
+    for i in range(len(products)):
+        output += str(coEffi[i + len(reactants)][0]) + products[i]
+        if i < len(products) - 1:
+            output += " + "
+    print(output)
